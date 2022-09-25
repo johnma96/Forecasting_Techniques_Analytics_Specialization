dd6cb5cabaf91fc424f3cbbe64dd965cd785b3e4
Mario Montoya <jmmontoyaz@unal.edu.co>
2022-09-25 12:29:21 -0500

    Automatic Holt Winters model development, with features for model selection and forecast quality assessment

--- Projects/project1/Project1_Models_ExpLineal_HoltWintersComp.R
+++ Projects/project1/Project1_Models_ExpLineal_HoltWintersComp.R
@@ -14,6 +14,42 @@ require(DescTools)
 # Remove variables on memory
 rm(list = ls())
 
+
+# Functions for evaluate statistics of model selection
+medidas.yest <- function(y, yest, k) {
+    # y = serie, m = modelo, k = numero parametros
+    T <- length(y)
+    sse <- sum((yest - y)^2)
+    ssr <- sum((y - mean(y))^2)
+    mse <- sse / (T - k)
+    R2 <- 1 - sse / ssr
+    Ra2 <- 1 - (T - 1) * (1 - R2) / (T - k)
+    aic <- log((T - k) * exp(2 * k / T) * mse / T)
+    bic <- log(T^(k / T) * (T - k) * mse / T)
+
+    M <- c(mse, sqrt(mse), Ra2, aic, bic)
+    names(M) <- c("mse", "rmse", "R2-ad", "log.aic", "log.bic")
+    return(M)
+}
+
+medidas <- function(m, y, k) {
+    # m = objeto producido con lm()
+    # y = variable dependiente
+    # k = numero de coeficientes beta
+    T <- length(y)
+    yest <- fitted(m)
+    sse <- sum((yest - y)^2)
+    ssr <- sum((y - mean(y))^2)
+    mse <- sse / (T - k)
+    R2 <- 1 - sse / ssr
+    Ra2 <- 1 - (T - 1) * (1 - R2) / (T - k)
+    aic <- log((T - k) * exp(2 * k / T) * mse / T)
+    bic <- log(T^(k / T) * (T - k) * mse / T)
+    M <- c(, mse, sqrt(mse), Ra2, aic, bic)
+    names(M) <- c("MSE", "RMSE", "R2-ajus", "logAIC", "logBIC")
+    return(M)
+}
+
 # Load data
 # Total quarterly beer production in Australia (in megalitres)
 # From 1956:Q1 to 2008:Q3
@@ -22,10 +58,12 @@ ts.plot(y)
 
 #----- cross-validation
 n <- length(y)
-len_yf <- 4 * 10 # 4 * num years for test data (10 in this case) aprox 80-20
+len_yf <- 4 * 3 # 4 * num years for test data (10 in this case) aprox 80-20
 yi <- ts(y[1:(n - len_yf)], start = c(time(y)[1]), frequency = 4)
-yf <- ts(y[(n - len_yf + 1):n], start = c(tail(time(yi), n = 1) + 0.25),
-         frequency = 4)
+yf <- ts(y[(n - len_yf + 1):n],
+    start = c(tail(time(yi), n = 1) + 0.25),
+    frequency = 4
+)
 
 
 str(n)
@@ -46,7 +84,7 @@ lines(yf, , type = "l", col = "red") # Plot second time series
 
 # Add legend to plot
 text_legend <- c("Train", "Test")
-legend("bottomright", 
+legend("bottomright",
     legend = text_legend, ,
     text.width = strwidth(text_legend)[1] * 2,
     lty = 1, ,
@@ -72,6 +110,10 @@ Tt <- model$fitted[, 2] + model$fitted[, 3]
 St <- model$fitted[, 4]
 Yt_hat <- model$fitted[, 1]
 
+# Statisticts model selection MSE, AIC, BIC R2-Adjust
+# source('medidas.yest.r')
+medidas.yest(yi, Yt_hat, 3) # OJO QUE DEVUELVE RMSE
+
 # Visually evaluate the fitted
 plot(yi,
     type = "l", col = "#000000", # Plot first time series
@@ -81,8 +123,10 @@ plot(yi,
 )
 lines(Yt_hat, , type = "l", col = "#f3a60d")
 
-# Forecast
-y_pron <- predict(model, 40, prediction.interval = TRUE)
+#----------------#
+#--- Forecast ---#
+#----------------#
+y_pron <- predict(model, len_yf, prediction.interval = TRUE)
 
 plot(y,
     type = "l", col = "black", # Plot first time series
@@ -92,43 +136,12 @@ plot(y,
 )
 lines(Yt_hat, , type = "l", col = "#f20505")
 
-#Visually evaluate the prediction
+# Visually evaluate the prediction
 lines(yf, , type = "l", col = "#000000")
-lines(y_pron[,1], , type = 'l', col = "orange")
+lines(y_pron[, 1], , type = "l", col = "orange")
 
-# Metric for
-R <- rbind(accuracy(yi, Yt_hat)) 
-rownames(R) <- c('HWC')
-
-
-
-
-
-
-
-
-
-
-
-#-------------------- Model 2 with auto-tunning of parameters-------------------#
-
-model1 <- HoltWinters(yi)
-param1 <- (c(model1$alpha, model1$beta, model1$gamma))
-
-# Trend, seasonal component and estimated Y
-Tt <- model1$fitted[, 2] + model1$fitted[, 3]
-St <- model1$fitted[, 4]
-Yt_hat <- model1$fitted[, 1]
-
-plot(Yt_hat,
-    type = "l", col = "#1164a3", # Plot first time series
-    main = "Y real vs modelo Holt-Winters Componentes",
-    xlab = "Año",
-    ylab = "Megalitros"
-)
-lines(yi, , type = "l", col = "red")
-
-# Forecast of 12
-y_pron <- predict(model1, 12, prediction.interval = TRUE)
-
-
+# Metric for quality forecast MAPE, RMSE, UTHEIL
+R <- rbind(accuracy(yf, y_pron[, 1]))[, c(2, 5)]
+Utheil <- c(TheilU(yf, y_pron[, 1]))
+R <- c(R, setNames(Utheil, "Utheil"))
+# rownames(R) <- c("HWC")
